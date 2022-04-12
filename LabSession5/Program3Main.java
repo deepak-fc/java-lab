8be9d78bb4b8f2be5a2446ba96db70cb54bd539b
deepak-fc <deepak.oli335@deccansociety.org>
2022-04-12 09:11:41 +0530

    fixed odd file writing issue

--- LabSession5/Program3Main.java
+++ LabSession5/Program3Main.java
@@ -2,19 +2,15 @@ import java.io.*;
 
 class Program3Main {
     public static void main(String[] args) throws IOException {
-
         String filePath = args[0];
-        String line;
 
+        String input;
+        String line;
         int currentLineNumber = 0;
 
-        char input;
-
-        BufferedReader inputTextFileReader = null;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedReader inputTextFileReader = null;
         FileReader inputTextFile = null;
-
         FileWriter evenTextFile = null;
         FileWriter oddTextFile = null;
 
@@ -28,11 +24,11 @@ class Program3Main {
 
                 while (true) {
                     System.out.print(">Do you want to overwrite it? [y/n]: ");
-                    input = (char) br.read();
-                    if (input == 'y' || input == 'Y') {
+                    input = br.readLine().trim().toLowerCase();
+                    if (input.equals("y")) {
                         evenTextFile = new FileWriter("even.txt");
                         break;
-                    } else if (input == 'n' || input == 'N') {
+                    } else if (input.equals("n")) {
                         evenTextFile = new FileWriter("even.txt", true);
                         break;
                     } else {
@@ -41,7 +37,7 @@ class Program3Main {
                 }
 
             } else {
-                System.out.println(">'even.txt' does not exixt. Creating new file...");
+                System.out.println(">'even.txt' does not exist. Creating new file...");
                 File f = new File("even.txt");
                 f.createNewFile();
                 evenTextFile = new FileWriter("even.txt");
@@ -51,12 +47,12 @@ class Program3Main {
                 System.out.println(">File 'odd.txt' already exists.");
 
                 while (true) {
-                    System.out.println(">Do you want to overwrite it? [y/n]: ");
-                    input = (char) br.read();
-                    if (input == 'y' || input == 'Y') {
+                    System.out.print(">Do you want to overwrite it? [y/n]: ");
+                    input = br.readLine().trim().toLowerCase();
+                    if (input.equals("y")) {
                         oddTextFile = new FileWriter("odd.txt");
                         break;
-                    } else if (input == 'n' || input == 'N') {
+                    } else if (input.equals("n")) {
                         oddTextFile = new FileWriter("odd.txt", true);
                         break;
                     } else {
@@ -65,14 +61,13 @@ class Program3Main {
                 }
 
             } else {
-                System.out.println(">'odd.txt' does not exixt. Creating new file...");
+                System.out.println(">'odd.txt' does not exist. Creating new file...");
                 File f = new File("odd.txt");
                 f.createNewFile();
                 oddTextFile = new FileWriter("odd.txt");
             }
 
             System.out.println("=> Copying data to respective files...");
-
             while ((line = inputTextFileReader.readLine()) != null) {
                 currentLineNumber++;
                 if (currentLineNumber % 2 == 0)
@@ -80,26 +75,26 @@ class Program3Main {
                 else
                     oddTextFile.write(line + "\n");
             }
-        
+
             System.out.println("=> Program ended...");
-        
+
         } catch (FileNotFoundException e) {
             System.out.println(">Error. Please provide correct path for 'input.txt'.");
         } finally {
+            if (evenTextFile != null)
+                evenTextFile.close();
+
+            if (oddTextFile != null)
+                oddTextFile.close();
+
             if (inputTextFileReader != null)
                 inputTextFileReader.close();
 
             if (inputTextFile != null)
                 inputTextFile.close();
-            
-            if(br != null)
-                br.close();
-            
-            if (evenTextFile != null)
-                evenTextFile.close();
 
-            if (evenTextFile != null)
-                evenTextFile.close();
+            if (br != null)
+                br.close();
         }
     }
 }
